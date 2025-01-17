25fca675c08c9e686438d0bd5877041eff4279fa
LucasGdosR <lucas.gribel.reis@gmail.com>
2025-01-17 09:08:05 -0300

    Improvement: worker pool pattern

--- 07/7.go
+++ 07/7.go
@@ -16,6 +16,20 @@ type handAndBid struct {
 	bid  int
 }
 
+type task struct {
+	tag         byte
+	start       int
+	end         int
+	sortingFunc func([]byte) (byte, byte)
+}
+
+const (
+	TASK_PARSE_SORT = iota
+	TASK_MERGE_SORT
+	TASK_COUNT
+	TASK_JUST_SORT
+)
+
 var cardToScore = map[byte]byte{'2': 1, '3': 2, '4': 3, '5': 4, '6': 5, '7': 6, '8': 7, '9': 8, 'T': 9, 'J': 10, 'Q': 11, 'K': 12, 'A': 13}
 
 func main() {
@@ -46,44 +60,36 @@ func rankHandsGetWinningsST() common.Results[int, int] {
 }
 
 func rankHandsGetWinningsMT() common.Results[int, int] {
-	// Variables
 	mappedFile := common.Mmap("input")
 	file := mappedFile.File
 	defer syscall.Munmap(file)
 	size := mappedFile.Size
 
 	numWorkers := runtime.GOMAXPROCS(0)
-	bytesPerWorker := size/int64(numWorkers) + 1
+
 	var wg sync.WaitGroup
+	taskChan := make(chan task, numWorkers)
 	partialSortedHands := make(chan []handAndBid, numWorkers)
 	partialWinnings := make(chan int, numWorkers)
+	var handsAndBids []handAndBid
+	for i := 0; i < numWorkers; i++ {
+		go worker(&wg, taskChan, partialSortedHands, partialWinnings, file, &handsAndBids)
+	}
+
+	bytesPerWorker := int(size)/numWorkers + 1
 	var winnings common.Results[int, int]
 
-	// Actual solution starts here
 	for i := 0; i < numWorkers; i++ {
-		start := int64(i) * bytesPerWorker
+		start := i * bytesPerWorker
 		end := start + bytesPerWorker
 		if i == numWorkers-1 {
-			end = size
+			end = int(size)
 		}
-		// Backtracking for getting lines out of Mmap
-		go func(start, end int64) {
-			if start > 0 {
-				for file[start-1] != '\n' {
-					start--
-				}
-			}
-			if end < int64(len(file)) {
-				for file[end-1] != '\n' {
-					end--
-				}
-			}
-			partialSortedHands <- parseAndSort(file, start, end)
-		}(start, end)
+		taskChan <- task{tag: TASK_PARSE_SORT, start: start, end: end}
 	}
-	handsAndBids := merge(&wg, numWorkers, partialSortedHands, countSameOfAKindRegular)
+	handsAndBids = merge(&wg, taskChan, numWorkers, partialSortedHands, countSameOfAKindRegular)
 	handsPerWorker := len(handsAndBids) / numWorkers
-	winnings.Part1 = getWinnings(numWorkers, handsPerWorker, handsAndBids, partialWinnings)
+	winnings.Part1 = getWinnings(numWorkers, handsPerWorker, handsAndBids, partialWinnings, taskChan)
 
 	// Part 2
 	cardToScore['J'] = 0
@@ -93,17 +99,13 @@ func rankHandsGetWinningsMT() common.Results[int, int] {
 		if i == numWorkers-1 {
 			end = len(handsAndBids)
 		}
-		go func(HABs []handAndBid) {
-			slices.SortFunc(HABs, func(a handAndBid, b handAndBid) int {
-				return sortingFunc(a, b, countSameOfAKindJoker)
-			})
-			partialSortedHands <- HABs
-		}(handsAndBids[start:end])
+		taskChan <- task{tag: TASK_JUST_SORT, start: start, end: end}
 	}
-	handsAndBids = merge(&wg, numWorkers, partialSortedHands, countSameOfAKindJoker)
+	handsAndBids = merge(&wg, taskChan, numWorkers, partialSortedHands, countSameOfAKindJoker)
 	close(partialSortedHands)
-	winnings.Part2 = getWinnings(numWorkers, handsPerWorker, handsAndBids, partialWinnings)
+	winnings.Part2 = getWinnings(numWorkers, handsPerWorker, handsAndBids, partialWinnings, taskChan)
 	close(partialWinnings)
+	close(taskChan)
 
 	cardToScore['J'] = 10 // So this can be benchmarked multiple times
 	return winnings
@@ -236,7 +238,50 @@ func parseInput() []handAndBid {
 	return handsAndBids
 }
 
-func parseAndSort(file []byte, start, end int64) []handAndBid {
+func worker(wg *sync.WaitGroup,
+	taskChan chan task,
+	sortedHands chan []handAndBid,
+	winnings chan int,
+	file []byte,
+	HABs *[]handAndBid) {
+	for t := range taskChan {
+		switch t.tag {
+		case TASK_PARSE_SORT:
+			// Backtracking for getting lines out of Mmap
+			if t.start > 0 {
+				for file[t.start-1] != '\n' {
+					t.start--
+				}
+			}
+			if t.end < len(file) {
+				for file[t.end-1] != '\n' {
+					t.end--
+				}
+			}
+			sortedHands <- parseAndSort(file, t.start, t.end)
+
+		case TASK_MERGE_SORT:
+			sortedHands <- mergeSort(sortedHands, t.sortingFunc)
+			wg.Done()
+
+		case TASK_COUNT:
+			totalWinnings := 0
+			for ; t.start < t.end; t.start++ {
+				totalWinnings += (t.start + 1) * (*HABs)[t.start].bid
+			}
+			winnings <- totalWinnings
+
+		case TASK_JUST_SORT:
+			HABsSlice := (*HABs)[t.start:t.end]
+			slices.SortFunc(HABsSlice, func(a handAndBid, b handAndBid) int {
+				return sortingFunc(a, b, countSameOfAKindJoker)
+			})
+			sortedHands <- HABsSlice
+		}
+	}
+}
+
+func parseAndSort(file []byte, start, end int) []handAndBid {
 	handsAndBids := make([]handAndBid, 0, 256) // tailored for 4 workers. could receive numworkers as parameter for initial capacity
 	for ; start < end; start++ {
 		var HAB handAndBid
@@ -255,16 +300,14 @@ func parseAndSort(file []byte, start, end int64) []handAndBid {
 
 func merge(
 	wg *sync.WaitGroup,
+	taskChan chan task,
 	numWorkers int,
 	partialSortedHands chan []handAndBid,
 	countSameOfAKind func([]byte) (byte, byte)) []handAndBid {
 
 	for i := 0; i < numWorkers-1; i++ {
 		wg.Add(1)
-		go func() {
-			partialSortedHands <- mergeSort(partialSortedHands, countSameOfAKind)
-			wg.Done()
-		}()
+		taskChan <- task{tag: TASK_MERGE_SORT, sortingFunc: countSameOfAKind}
 	}
 	wg.Wait()
 	return <-partialSortedHands
@@ -290,20 +333,15 @@ func mergeSort(partialSortedHands chan []handAndBid, countSameOfAKind func([]byt
 }
 
 func getWinnings(numWorkers, handsPerWorker int,
-	HABs []handAndBid, partialWinnings chan int) int {
+	HABs []handAndBid, partialWinnings chan int,
+	taskChan chan task) int {
 	for i := 0; i < numWorkers; i++ {
 		start := i * handsPerWorker
 		end := start + handsPerWorker
 		if i == numWorkers-1 {
 			end = len(HABs)
 		}
-		go func(start, end int) {
-			totalWinnings := 0
-			for ; start < end; start++ {
-				totalWinnings += (start + 1) * HABs[start].bid
-			}
-			partialWinnings <- totalWinnings
-		}(start, end)
+		taskChan <- task{tag: TASK_COUNT, start: start, end: end}
 	}
 	var winnings int
 	for i := 0; i < numWorkers; i++ {
