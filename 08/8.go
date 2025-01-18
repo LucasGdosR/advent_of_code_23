c17d4065f718f683a87878410d410316232c475a
LucasGdosR <lucas.gribel.reis@gmail.com>
2025-01-18 07:56:12 -0300

    Day 8 multi-threaded solution

--- 08/8.go
+++ 08/8.go
@@ -3,7 +3,6 @@ package main
 import (
 	"aoc/2023/common"
 	"bufio"
-	"fmt"
 )
 
 type key struct {
@@ -12,7 +11,64 @@ type key struct {
 }
 
 func main() {
+	thisProgram := common.Benchmarkee[int, int]{
+		ST_Impl:  countStepsToExitST,
+		MT_Impl:  countStepsToExitMT,
+		Part1Str: "Steps to exit",
+		Part2Str: "Steps to exit",
+	}
+	common.Benchmark(thisProgram, 1000)
+}
+
+func countStepsToExitST() common.Results[int, int] {
+	LR, LRMap, startingPoints := parseInput()
+
+	l := len(LR)
+	results := common.Results[int, int]{Part2: l}
+	for _, start := range startingPoints {
+		count := 0
+		for curr := start; curr[2] != 'Z'; count++ {
+			curr = LRMap[key{str: curr, b: LR[count%l]}]
+		}
+		if start == "AAA" {
+			results.Part1 = count
+		}
+		results.Part2 *= count / l
+	}
+
+	return results
+}
+
+func countStepsToExitMT() common.Results[int, int] {
+	// This could be done in parallel, but it's not worth the trouble
+	LR, LRMap, startingPoints := parseInput()
+
+	l, points := len(LR), len(startingPoints)
+	results := common.Results[int, int]{Part2: l}
+	counts := make(chan int, len(startingPoints))
+	for _, start := range startingPoints {
+		go func() {
+			count := 0
+			for curr := start; curr[2] != 'Z'; count++ {
+				curr = LRMap[key{str: curr, b: LR[count%l]}]
+			}
+			if start == "AAA" {
+				results.Part1 = count
+			}
+			counts <- count / l
+		}()
+	}
+	for i := 0; i < points; i++ {
+		results.Part2 *= <-counts
+	}
+	close(counts)
+
+	return results
+}
+
+func parseInput() (string, map[key]string, []string) {
 	file := common.Open("input")
+	defer file.Close()
 	scanner := bufio.NewScanner(file)
 	scanner.Scan()
 	LR := scanner.Text()
@@ -29,20 +85,5 @@ func main() {
 			startingPoints = append(startingPoints, curr)
 		}
 	}
-
-	var part1Count int
-	l := len(LR)
-	part2Count := l
-	for _, start := range startingPoints {
-		count := 0
-		for curr := start; curr[2] != 'Z'; count++ {
-			curr = LRMap[key{str: curr, b: LR[count%l]}]
-		}
-		if start == "AAA" {
-			part1Count = count
-		}
-		part2Count *= count / l
-	}
-
-	fmt.Println(part1Count, part2Count)
+	return LR, LRMap, startingPoints
 }
