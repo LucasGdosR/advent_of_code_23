c033ac81ed74967d8bad6df49deab4a78a1a7576
LucasGdosR <lucas.gribel.reis@gmail.com>
2025-01-21 06:50:22 -0300

    Day 10 MT solution

--- 10/10-ray-casting.go
+++ 10/10-ray-casting.go
@@ -3,12 +3,14 @@ package main
 import (
 	"aoc/2023/common"
 	"bufio"
+	"runtime"
 )
 
 type Direction byte
 
 const (
-	Up Direction = iota
+	GRID_SIDE           = 140
+	Up        Direction = iota
 	Right
 	Down
 	Left
@@ -24,15 +26,51 @@ type state struct {
 }
 
 func main() {
-	file := common.Open("input")
-	scanner := bufio.NewScanner(file)
-	pipes := make([][]byte, 0, 140)
-	for scanner.Scan() {
-		bytes := make([]byte, 140)
-		copy(bytes, scanner.Bytes())
-		pipes = append(pipes, bytes)
+	thisProgram := common.Benchmarkee[int, int]{
+		ST_Impl:  findCycleAndNestSpotsST,
+		MT_Impl:  findCycleAndNestSpotsMT,
+		Part1Str: "Steps to the middle of the cycle",
+		Part2Str: "Possible nest spots",
+	}
+	common.Benchmark(thisProgram, 1000)
+}
+
+func findCycleAndNestSpotsST() common.Results[int, int] {
+	pipes := readPipeMaze()
+	return common.Results[int, int]{
+		Part1: findCycle(pipes) / 2,
+		Part2: findNestSpots(pipes)}
+}
+
+func findCycleAndNestSpotsMT() common.Results[int, int] {
+	pipes := readPipeMaze()
+	cycleLength := findCycle(pipes)
+
+	numWorkers := runtime.GOMAXPROCS(0)
+	linesPerWoker := (GRID_SIDE + numWorkers - 1) / numWorkers
+	partialNestSpots := make(chan int, numWorkers)
+
+	for i := 0; i < numWorkers; i++ {
+		start := i * linesPerWoker
+		end := start + linesPerWoker
+		if i == numWorkers-1 {
+			end = GRID_SIDE
+		}
+		go func(start, end int) {
+			partialNestSpots <- findNestSpots(pipes[start:end])
+		}(start, end)
 	}
 
+	results := common.Results[int, int]{Part1: cycleLength / 2}
+	for i := 0; i < numWorkers; i++ {
+		results.Part2 += <-partialNestSpots
+	}
+	close(partialNestSpots)
+
+	return results
+}
+
+func findCycle(pipes [][]byte) int {
 	start := findStart(pipes)
 
 	s := state{p: start, d: Up}
@@ -42,22 +80,7 @@ func main() {
 	for ; s.p != start; count++ {
 		s = next(pipes, s)
 	}
-
-	var nestSpots int
-	for _, row := range pipes {
-		var isEnclosed bool
-		for _, c := range row {
-			if c == '#' {
-				isEnclosed = !isEnclosed
-			} else if isEnclosed && c != '@' {
-				nestSpots++
-			}
-		}
-	}
-
-	println(count / 2)
-
-	println(nestSpots)
+	return count
 }
 
 func next(pipes [][]byte, s state) state {
@@ -119,6 +142,21 @@ func next(pipes [][]byte, s state) state {
 	return s
 }
 
+func findNestSpots(pipes [][]byte) int {
+	var nestSpots int
+	for _, row := range pipes {
+		var isEnclosed bool
+		for _, c := range row {
+			if c == '#' {
+				isEnclosed = !isEnclosed
+			} else if isEnclosed && c != '@' {
+				nestSpots++
+			}
+		}
+	}
+	return nestSpots
+}
+
 func findStart(pipes [][]byte) p {
 	for i, row := range pipes {
 		for j, c := range row {
@@ -128,4 +166,17 @@ func findStart(pipes [][]byte) p {
 		}
 	}
 	return p{i: -1, j: -1}
+}
+
+func readPipeMaze() [][]byte {
+	file := common.Open("input")
+	defer file.Close()
+	scanner := bufio.NewScanner(file)
+	pipes := make([][]byte, 0, GRID_SIDE)
+	for scanner.Scan() {
+		bytes := make([]byte, GRID_SIDE)
+		copy(bytes, scanner.Bytes())
+		pipes = append(pipes, bytes)
+	}
+	return pipes
 }
