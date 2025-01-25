d9aafa20dfa4ada20d59b9e1487fddb48e90b8c1
LucasGdosR <lucas.gribel.reis@gmail.com>
2025-01-25 05:53:47 -0300

    Day 12 MT solution

--- 12/12.go
+++ 12/12.go
@@ -6,25 +6,65 @@ import (
 	"strings"
 )
 
-var memo = make(map[string]int)
-
 func main() {
+	thisProgram := common.Benchmarkee[int, int]{
+		ST_Impl:  countPossibleArragementsST,
+		MT_Impl:  countPossibleArragementsMT,
+		Part1Str: "Possible arrangements",
+		Part2Str: "Possible arrangements",
+	}
+	common.Benchmark(thisProgram, 100)
+}
+
+func countPossibleArragementsST() common.Results[int, int] {
 	conditionRecords := common.Open("input")
 	defer conditionRecords.Close()
 
+	memo := make(map[string]int, 260417)
 	var results common.Results[int, int]
 	scanner := bufio.NewScanner(conditionRecords)
 	for scanner.Scan() {
 		line := strings.Fields(scanner.Text())
 		dotsNHashes, nums := line[0], line[1]
-		results.Part1 += possibleArrangements(dotsNHashes, nums)
-		results.Part2 += possibleArrangements(unfold(dotsNHashes, nums))
+		results.Part1 += possibleArrangements(dotsNHashes, nums, memo)
+		UDND, UNums := unfold(dotsNHashes, nums)
+		results.Part2 += possibleArrangements(UDND, UNums, memo)
 	}
 
-	println(results.Part1, results.Part2)
+	return results
 }
 
-func possibleArrangements(dnd, nums string) int {
+func countPossibleArragementsMT() common.Results[int, int] {
+	return common.SolveCommonCaseMmapLinesInt(countRangeArrangements)
+}
+
+func countRangeArrangements(data []byte, start int64, end int64) common.Results[int, int] {
+	var partial common.Results[int, int]
+	memo := make(map[string]int, 260417)
+	for start < end {
+		whitespace := start + 10
+		for data[whitespace] != ' ' {
+			whitespace++
+		}
+		dotsNHashes := string(data[start:whitespace])
+
+		lineEnd := whitespace + 4
+		for lineEnd < end && data[lineEnd] != '\n' {
+			lineEnd++
+		}
+		nums := (string(data[whitespace+1 : lineEnd]))
+
+		partial.Part1 += possibleArrangements(dotsNHashes, nums, memo)
+		UDND, UNums := unfold(dotsNHashes, nums)
+		partial.Part2 += possibleArrangements(UDND, UNums, memo)
+
+		start = lineEnd + 1
+	}
+
+	return partial
+}
+
+func possibleArrangements(dnd, nums string, memo map[string]int) int {
 	key := dnd + nums
 	if v, ok := memo[key]; ok {
 		return v
@@ -44,17 +84,17 @@ func possibleArrangements(dnd, nums string) int {
 	var count int
 	switch dnd[0] {
 	case '.':
-		count = possibleArrangements(dnd[1:], nums)
+		count = possibleArrangements(dnd[1:], nums, memo)
 	case '#':
-		count = pound(dnd, nums)
+		count = pound(dnd, nums, memo)
 	case '?':
-		count = possibleArrangements(dnd[1:], nums) + pound(dnd, nums)
+		count = possibleArrangements(dnd[1:], nums, memo) + pound(dnd, nums, memo)
 	}
 	memo[key] = count
 	return count
 }
 
-func pound(dnd, nums string) int {
+func pound(dnd, nums string, memo map[string]int) int {
 	var num int
 	i := strings.IndexByte(nums, ',')
 	if i == -1 {
@@ -74,9 +114,9 @@ func pound(dnd, nums string) int {
 		return 0
 	} else {
 		if i == -1 {
-			return possibleArrangements(dnd[num+1:], "")
+			return possibleArrangements(dnd[num+1:], "", memo)
 		} else {
-			return possibleArrangements(dnd[num+1:], nums[i+1:])
+			return possibleArrangements(dnd[num+1:], nums[i+1:], memo)
 		}
 	}
 }
